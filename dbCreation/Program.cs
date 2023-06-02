3be4bd57681c98ea182e66a21e44379473c09c3b
DonBravisti <butov.s-butov@yandex.ru>
2023-06-02 17:39:30 +0300

    Добавил insert, update, select

--- dbCreation/Program.cs
+++ dbCreation/Program.cs
@@ -20,7 +20,7 @@ namespace dbCreation
 				Console.WriteLine("opened...");
 
 				string queryCreateDB = "CREATE SCHEMA IF NOT EXISTS `my_db`;" +
-								"USE my_db";
+									   "USE my_db";
 				MySqlCommand command = new MySqlCommand(queryCreateDB, connection);
 				command.ExecuteNonQuery();
 
@@ -43,17 +43,47 @@ namespace dbCreation
 					"name VARCHAR(255) NOT NULL," +
 					"department_id int,"+
 					"FOREIGN KEY(department_id) REFERENCES departments(id))";
+
+
 				command = new MySqlCommand(queryCreateUsers, connection);
 				command.ExecuteNonQuery();
 
-				string queryInsertDocs = "INSERT INTO `my_db`.`documents` (`title`) VALUES ('doc1')";
-				string queryInsertDeps = "INSERT INTO `my_db`.`departments` (`title`, `document_id`) VALUES ('doc1')";
-				string queryInsertUsers = "INSERT INTO `my_db`.`users` (`name`, `department_id`) VALUES ('ted', '1')";
+				string queryInsert = @"INSERT INTO `my_db`.`documents` (`title`) VALUES ('doc1');
+									   INSERT INTO `my_db`.`documents` (`title`) VALUES ('doc2');
 
-				command = new MySqlCommand(queryInsertDocs, connection);
-				command = new MySqlCommand(queryInsertDeps, connection);
-				command = new MySqlCommand(queryInsertUsers, connection);
-			}
+									   INSERT INTO `my_db`.`departments` (`title`, `document_id`) VALUES ('dep1', '1');
+									   INSERT INTO `my_db`.`departments` (`title`, `document_id`) VALUES ('dep2', '2');
+
+									   INSERT INTO `my_db`.`users` (`name`, `department_id`) VALUES ('ted', '1');
+									   INSERT INTO `my_db`.`users` (`name`, `department_id`) VALUES ('barney', '2');";
+				command = new MySqlCommand(queryInsert, connection);
+				command.ExecuteNonQuery();
+
+				string queryUpdate = @"UPDATE `my_db`.`documents` SET `title` = 'docdo' WHERE (`id` = '1');";
+                command = new MySqlCommand(queryUpdate, connection);
+                command.ExecuteNonQuery();
+
+				string querySelect = @"SELECT
+									   u.id as ""id пользователя"",
+									   u.name as ""Имя"",
+									   dep.title as ""Отдел"",
+									   doc.title as ""Расписание""
+									   FROM my_db.users as u
+									   join my_db.departments as dep on dep.id = u.department_id
+									   join my_db.documents as doc on doc.id = dep.document_id";
+                command = new MySqlCommand(querySelect, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+				while (reader.Read())
+				{
+					object id = reader.GetValue(0);
+                    object name = reader.GetValue(1);
+                    object dep = reader.GetValue(2);
+                    object doc = reader.GetValue(3);
+
+					string result = string.Format("{0} {1} {2} {3}", id, name, dep, doc);
+                    Console.WriteLine(result);
+                }
+            }
 			catch (MySqlException ex)
 			{
 				Console.WriteLine(ex.Message);
